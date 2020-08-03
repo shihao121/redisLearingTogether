d9cf3bcd97791d0933fcc735281122391a4b2186
yangkaiguang <thoughtworks.yang@daimler.com>
2020-08-03 11:06:35 +0800

    use redis cache in api

--- src/main/java/com/north/learning/redis/controller/RedisController.java
+++ src/main/java/com/north/learning/redis/controller/RedisController.java
@@ -1,14 +1,25 @@
 package com.north.learning.redis.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class RedisController {
+
+    private final RedisTemplate redisTemplate;
+
 
     @GetMapping(value = "/hello")
     public String greeting(@RequestParam(value = "name") String name) {
+        String cacheName = (String) redisTemplate.opsForValue().get(name);
+        if (cacheName != null) {
+            return "hello again, " + cacheName;
+        }
+        redisTemplate.opsForValue().set(name, name);
         System.out.println("use new person name");
         return "nice to meet you, " + name;
     }
